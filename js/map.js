190df6a3b8f9706872400afe42ba8165da217e6e
Chloe Yeo <yeochloe01@gmail.com>
2024-04-14 09:03:25 +0100

    added href links up to second swiper items

--- js/map.js
+++ js/map.js
@@ -68,6 +68,8 @@ document.addEventListener("DOMContentLoaded", function () {
         return false;
       }
 
+      // const currentCoordinate
+
       // 장소검색 객체를 통해 키워드로 장소검색을 요청합니다
       ps.keywordSearch(keyword, placesSearchCB);
     }
